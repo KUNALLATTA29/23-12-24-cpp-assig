03fda181c6988dbca13c5ebd7781f7c8adf7b4f3
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-12-23 20:45:45 +0530

    Create prog_7.cpp

---
+++ prog_7.cpp
@@ -0,0 +1,23 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int arr[7] = {2,3,1,6,4,9,12};
+    int ans[7];
+    int size = sizeof(arr)/sizeof(arr[0]);
+    
+
+    for(int i=0;i<size;i++){
+        int k = arr[i]+arr[i+1];
+        ans[i] = k;
+    }
+   
+    for(auto it:ans){
+        cout<<it<<" ";
+    }
+    
+    return 0;
+}
