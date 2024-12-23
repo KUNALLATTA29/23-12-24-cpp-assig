a03522340ae4124d265a15f4718ec002cf6e3b98
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-12-23 22:58:40 +0530

    Create prog_14.cpp

---
+++ prog_14.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    vector<int> arr = {2,5,4,9,3,5,4,7};
+    vector<float> ans;
+    int size = arr.size();
+    
+
+    for(int i=0;i<size;i++){
+        ans.push_back(arr[i]-2);
+    }
+   
+    for(auto it:ans){
+        cout<<it<<" ";
+    }
+    
+    return 0;
+}
